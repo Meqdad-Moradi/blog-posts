dbff3c96bab203049b3cb7d090b0a1b7839f95de
Meqdad-Moradi <meqdad.moradi@outlook.com>
2024-12-12 18:12:01 +0100

    components and routing created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: NotFoundComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
