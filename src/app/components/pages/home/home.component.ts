bc876c882805f3ca97ef5bc38174201120eefc09
Meqdad-Moradi <meqdad.moradi@outlook.com>
2024-12-22 22:39:07 +0100

    Home sections added

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { ListComponent } from "../../blog-posts/list/list.component";
-import { ShowcaseComponent } from "../../shared/showcase/showcase.component";
+import { ListComponent } from '../../blog-posts/list/list.component';
+import { ShowcaseComponent } from '../../shared/showcase/showcase.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [ListComponent, ShowcaseComponent],
+  imports: [ListComponent, ShowcaseComponent, RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
