57aac7d5d82c1a6c270cfeabffb5c686022c3e48
Meqdad-Moradi <meqdad.moradi@outlook.com>
2024-12-23 09:43:15 +0100

    contac UI has been created

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ShowcaseComponent } from '../../shared/showcase/showcase.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
-  imports: [ShowcaseComponent],
+  imports: [ShowcaseComponent, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
